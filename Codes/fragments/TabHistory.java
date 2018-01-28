e6ddb244718aa3baef11a6c7a7ef526e4176eb15
Petri Mattila <Etherkon@users.noreply.github.com>
2018-01-28 18:18:25 +0200

    Create TabHistory.java

---
+++ Codes/fragments/TabHistory.java
@@ -0,0 +1,30 @@
+package com.bizfit.bizfit.fragments;
+
+/**
+ * Created by Käyttäjä on 7.2.2016.
+ */
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.bizfit.bizfit.R;
+
+public class TabHistory extends Fragment {
+
+    public TabHistory() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.tab_fragment_3, container, false);
+    }
+}
